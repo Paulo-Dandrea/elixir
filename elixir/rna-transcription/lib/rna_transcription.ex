968abc650559626119ab3c5a4f1bb427581dd603
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-08-18 17:18:38 -0300

    use codepoints correctly

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -1,29 +1,16 @@
 defmodule RnaTranscription do
-  @doc """
-  Transcribes a character list representing DNA nucleotides to RNA
-
-  ## Examples
-
-  iex> RnaTranscription.to_rna('ACTG')
-  'UGAC'
-  """
   @spec to_rna(String.t()) :: String.t()
   def to_rna_char(char) do
     case char do
-      "G" -> "C"
-      "C" -> "G"
-      "T" -> "A"
-      "A" -> "U"
-      _ -> char
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end
   end
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    to_string(dna)
-    |> String.graphemes()
-    |> Enum.map(&RnaTranscription.to_rna_char/1)
-    |> Enum.join()
-    |> String.to_charlist()
+    Enum.map(dna, &RnaTranscription.to_rna_char/1)
   end
 end
