dc137115e5e6a6032e79c310267b539d47e917ac
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-08-19 06:03:09 -0300

    add struct to hold hex values for our new Identicon generator

---
+++ tuts/identicon/lib/identicon.ex
@@ -0,0 +1,18 @@
+defmodule Identicon do
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def pick_color(image) do
+
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
