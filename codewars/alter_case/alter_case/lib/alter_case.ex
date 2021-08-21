eb302334778a9c1b59281993b60c269bc7dfb5c0
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-08-21 08:07:53 -0300

    alter case alternatives

--- codewars/alter_case/alter_case/lib/alter_case.ex
+++ codewars/alter_case/alter_case/lib/alter_case.ex
@@ -17,3 +17,35 @@ defmodule Codewars.StringUtils do
     |> Enum.join("")
   end
 end
+
+ # Another good solutions to learn
+  # def rotate(text, shift) do
+
+
+defmodule Codewars.StringUtils do
+  @d ?a - ?A
+
+  def alter_case(str) do
+    str
+    |> String.to_char_list
+    |> Enum.map(&( _alter(&1)) )
+    |> List.to_string
+  end
+
+  defp _alter(c) when c in ?a..?z, do: c - @d
+  defp _alter(c) when c in ?A..?Z, do: c + @d
+  defp _alter(c), do: c
+end
+
+
+defmodule Codewars.StringUtils do
+  def alter_case(str) do
+    import String
+    import Enum
+
+    str
+    |> codepoints
+    |> map(&(if &1 == upcase(&1), do: downcase(&1), else: upcase(&1)))
+    |> to_string
+  end
+end
