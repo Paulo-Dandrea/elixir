6ca251b02c2469be53177583a3ed9eb9cb7c12cd
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-08-23 20:54:49 -0300

    recursive solution to Roman Numerals

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,25 +1,19 @@
 defmodule RomanNumerals do
-  @doc """
-  Convert the number to a roman number.
-  """
-  # @spec numeral(pos_integer) :: String.t()
-  # def numeral(number, roman \\ "") do
-  #   # if number >= 1000 do
-  #   #   # Io.put(number)
-  #   #   roman = roman <> "M"
-  #   #   numeral(number - 1000, roman <> "M")
-  #   # end
-  #   case number do
-  #     >= 1000 -> numeral(0, "I")
-  #     < 0 -> roman
-  #   end
-  # end
-
-  def numeral(number, roman \\ "") when number >= 1000, do: numeral(number, roman <> "M")
-  def numeral(number, roman \\ "") when number <= 0, do: roman
-
-
-  # def numeral(0, roman) do
-  #   roman
-  # end
-end
+  def numeral(number, roman \\ "") do
+    cond do
+      number >= 1000 -> numeral(number - 1000, roman <> "M")
+      number >= 900 -> numeral(number - 900, roman <> "CM")
+      number >= 500 -> numeral(number - 500, roman <> "D")
+      number >= 400 -> numeral(number - 400, roman <> "CD")
+      number >= 100 -> numeral(number - 100, roman <> "C")
+      number >= 90 -> numeral(number - 90, roman <> "XC")
+      number >= 50 -> numeral(number - 50, roman <> "L")
+      number >= 40 -> numeral(number - 40, roman <> "XL")
+      number >= 10 -> numeral(number - 10, roman <> "X")
+      number >= 9 -> numeral(number - 9, roman <> "IX")
+      number >= 5 && number <= 8 -> numeral(number - 5, roman <> "V")
+      number >= 4 -> numeral(number - 4, roman <> "IV")
+      number >= 1 && number <= 3 -> numeral(number - 1, roman <> "I")
+      number <= 0 -> roman
+    end
+  end
